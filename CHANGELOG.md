a92703c3dcc477397274ee14f70f17156b9911e9
ThanCoder <thancoder2019@gmail.com>
2025-08-06 13:56:30 +0630

    fixed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 0.8.1
+
+## Fixed
+- `SeeAllView`
+
 # 0.8.0
 
 ## Added Widgets
--- lib/views/t_see_all_view.dart
+++ lib/views/t_see_all_view.dart
@@ -1,84 +1,132 @@
 import 'package:flutter/material.dart';
 
-class TSeeAllView<T> extends StatelessWidget {
+class SeeAllView<T> extends StatelessWidget {
   List<T> list;
   String title;
-  Color? titleColor;
-  Widget moreTitle;
+  String moreTitle;
   int showCount;
-  int? showLines;
   double fontSize;
   double padding;
   EdgeInsetsGeometry? margin;
+  Color? titleColor;
   void Function(String title, List<T> list) onSeeAllClicked;
-  Widget? Function(BuildContext context, T item) gridItemBuilder;
+  Widget Function(BuildContext context, T item) gridItemBuilder;
+  double itemHeight;
+  double itemWidth;
+  bool showMoreButtonBottomPos;
 
-  TSeeAllView({
+  SeeAllView({
     super.key,
     required this.title,
     required this.list,
     required this.onSeeAllClicked,
     required this.gridItemBuilder,
-    this.moreTitle = const Text('More', style: TextStyle(color: Colors.blue)),
     this.showCount = 4,
     this.margin,
-    this.showLines = 1,
     this.fontSize = 11,
     this.padding = 6,
+    this.moreTitle = 'More',
     this.titleColor,
+    this.itemHeight = 200,
+    this.itemWidth = 160,
+    this.showMoreButtonBottomPos = false,
   });
 
   @override
   Widget build(BuildContext context) {
     final showList = list.take(showCount).toList();
-    if (showList.isEmpty) return const SizedBox.shrink();
-    int showLine = 1;
-    if (showLines == null && showList.length > 1) {
-      showLine = 2;
-    } else {
-      showLine = showLines ?? 1;
+    if (showList.isEmpty) {
+      return SizedBox.shrink();
     }
 
-    return Container(
-      padding: EdgeInsets.all(padding),
-      margin: margin,
-      child: SizedBox(
-        height: showLine * 170,
-        child: Column(
-          spacing: 5,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(title, style: TextStyle(color: titleColor)),
-                list.length > showCount
-                    ? Container(
-                      margin: const EdgeInsets.only(right: 5),
-                      child: TextButton(
-                        onPressed: () => onSeeAllClicked(title, list),
-                        child: moreTitle,
-                      ),
-                    )
-                    : const SizedBox.shrink(),
-              ],
-            ),
-            Expanded(
-              child: GridView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: showList.length,
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 170,
-                  mainAxisExtent: 130,
-                  mainAxisSpacing: 5,
-                  crossAxisSpacing: 5,
-                ),
-                itemBuilder:
-                    (context, index) => gridItemBuilder(context, list[index]),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 5,
+      children: [
+        // header row
+        _getHeader(),
+        // grid item
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            spacing: 5,
+            children: List.generate(
+              showList.length,
+              (index) => MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: SizedBox(
+                    width: itemWidth,
+                    height: itemHeight,
+                    child: gridItemBuilder(context, showList[index])),
               ),
             ),
-          ],
+          ),
         ),
+        // botton nav
+        _getSeeAllButtonBottomPos(),
+      ],
+    );
+  }
+
+  Widget _getHeader() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title, style: TextStyle(color: titleColor)),
+          _getSeeAllButton(),
+        ],
       ),
     );
+  }
+
+  Widget _getSeeAllButtonBottomPos() {
+    if (!showMoreButtonBottomPos) {
+      return const SizedBox.shrink();
+    }
+    if (list.length > showCount) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SizedBox(height: 15),
+          TextButton(
+              onPressed: () => onSeeAllClicked(title, list),
+              child: Text(
+                'See All',
+                style: TextStyle(color: Colors.blue),
+              ))
+        ],
+      );
+    }
+    return const SizedBox.shrink();
+  }
+
+  Widget _getSeeAllButton() {
+    if (list.length > showCount) {
+      return Container(
+        margin: const EdgeInsets.only(right: 2),
+        child: GestureDetector(
+          onTap: () => onSeeAllClicked(title, list),
+          child: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: Row(
+              spacing: 2,
+              children: [
+                Text(
+                  moreTitle,
+                  style: const TextStyle(color: Colors.blue),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 15,
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+    return const SizedBox.shrink();
   }
 }
